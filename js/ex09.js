78862390ea03593af90cc62219b76810add34ca8
wangjingaaa <2350905210@qq.com>
2017-05-18 13:37:29 +0800

    add ex09.html

---
+++ js/ex09.js
@@ -0,0 +1,14 @@
+var map = new BMap.Map("myMap");
+var point = new BMap.Point(120.156,30.249);
+map.centerAndZoom(point, 15);
+
+ 
+map.addControl(new BMap.NavigationControl());   
+map.addControl(new BMap.ScaleControl());    
+map.addControl(new BMap.OverviewMapControl());    
+map.addControl(new BMap.MapTypeControl()); 
+map.enableScrollWheelZoom();   
+map.setCurrentCity("杭州"); 
+
+var marker = new BMap.Marker(point);// 创建标注    
+map.addOverlay(marker);
